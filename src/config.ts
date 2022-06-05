1007c3d0011c2869ac555a0e07412e1960aa21bc
jadnw <jadenwu137@protonmail.com>
2022-06-05 22:21:00 +0700

    :hammer: Fix title

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import { Scale, Types, AUTO } from 'phaser'
 
 const config: Types.Core.GameConfig = {
-  title: 'Jay The Coder',
+  title: 'Phaser 3 With TypeScript, Rollup, Vite Template',
   type: AUTO,
   parent: 'game',
   backgroundColor: '#33A5E7',
